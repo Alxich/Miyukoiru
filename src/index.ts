0c1ca5a378288449da0d912d0449c53939ecb83f
Alxich <belich.olexandr@gmail.com>
2023-08-30 16:32:59 +0100

    Change back

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = async () => {
   dotenv.config();
 
   const bot: Telegraf<Context<Update>> = new Telegraf(
-    "6043588883:AAHD6PF7q8x64eFT7zZFv0rgw-C0tjIaMoA"
+    process.env.TELLEGRAM_TOKEN as string
   );
 
   const tsundere = new Tsundere("UA");
